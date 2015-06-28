4946443bb652f99337e74b053f25542555cda587
Bohdan Kucheriaviy <bohdan.kucheriavuy@gmail.com>
2015-06-28 15:49:05 +0300

    Chapter#18 - Views, WorkingWithRazor

---
+++ Chapter#18/WorkingWithRazor/WorkingWithRazor/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+
+namespace WorkingWithRazor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public string[] ListOfFruits { get; set; }
+
+        public HomeController()
+        {
+            ListOfFruits = new[] {"Orange", "Apple", "Banana"};
+        }
+
+        public ActionResult Index()
+        {
+            return View(this);
+        }
+
+        public ActionResult List()
+        {
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult Time()
+        {
+            return PartialView(DateTime.Now);
+        }
+    }
+}
