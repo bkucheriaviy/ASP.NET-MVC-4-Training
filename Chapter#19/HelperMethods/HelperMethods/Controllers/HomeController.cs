7adb86ce4df399f77aa9a1cfe7d09c663ccbf666
Bohdan Kucheriaviy <bohdan.kucheriavuy@gmail.com>
2015-06-28 19:40:03 +0300

    Chapter#19 - HelperMethods

---
+++ Chapter#19/HelperMethods/HelperMethods/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System.Web.Mvc;
+using HelperMethods.Models;
+
+namespace HelperMethods.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult InnerHelperMethod()
+        {
+            ViewBag.Fruits = new[] {"Apple", "Mango", "Orange"};
+            ViewBag.Cities = new[] {"New York", "London", "Paris"};
+            return View();
+        }
+        public ActionResult ExternalHelperMethod()
+        {
+            ViewBag.Fruits = new[] { "Apple", "Mango", "Orange" };
+            ViewBag.Cities = new[] { "New York", "London", "Paris" };
+            return View();
+        }
+
+        public ActionResult InputElementEncoding()
+        {
+            return View((object)"This is an HTML element: <input/>");
+        }
+
+        public ActionResult FormHelperMethod()
+        {
+            return View(new Person());
+        }
+
+        [HttpPost]
+        public ActionResult FormHelperMethod(Person person)
+        {
+            return View(person);
+        }
+    }
+}
---
+++ Chapter#19/HelperMethods/HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+
+namespace HelperMethods.Infrastructure
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString ListArrayItems(this HtmlHelper htmlHelper, IEnumerable<string> items)
+        {
+            var ul = new TagBuilder("ul");
+            foreach (var item in items)
+            {
+                var li = new TagBuilder("li");
+                li.SetInnerText(item);
+                ul.InnerHtml += li.ToString();
+            }
+            return new MvcHtmlString(ul.ToString());
+        }
+
+        public static MvcHtmlString DisplayMessage(this HtmlHelper htmlHelper, string message)
+        {
+            return new MvcHtmlString(string.Format("This is the message: <p>{0}</p>", message));
+        }
+        public static MvcHtmlString DisplayMessageFixed(this HtmlHelper htmlHelper, string message)
+        {
+            var encodedMessage = htmlHelper.Encode(message);
+            return new MvcHtmlString(string.Format("This is the message: <p>{0}</p>", encodedMessage));
+        }
+    }
+}
