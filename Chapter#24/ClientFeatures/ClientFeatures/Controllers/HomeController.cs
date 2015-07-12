fc45d8ea5252586181446e478683777590b242cf
Bohdan Kucheriaviy <bohdan.kucheriavuy@gmail.com>
2015-07-12 19:25:57 +0300

    Chapter#24 - ClientFeatures(bundles & load optimization)

---
+++ Chapter#24/ClientFeatures/ClientFeatures/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Web.Mvc;
+using ClientFeatures.Models;
+
+namespace ClientFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult MakeBooking()
+        {
+            return View(new Appointment
+            {
+                ClientName = "Adam",
+                Date = DateTime.Now.AddDays(2),
+                TermsAccepted = true
+            });
+        }
+
+        [HttpPost]
+        public JsonResult MakeBooking(Appointment app)
+        {
+            return Json(app, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
